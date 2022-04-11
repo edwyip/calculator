09000509aef06e14259745bcf19c1c14702577d9
edwyip <clyip93@gmail.com>
2022-04-12 03:44:21 +0800

    created buttons for numbers and basic calculations

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const add = function(a, b) {
+function add(a, b) {
 	return a+b;
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
 	return a-b;
 };
 
-const multiply = function(myArray) {
+function multiply(myArray) {
     return myArray.reduce((total, num)=> total *=  num, 1)
     };
 
-const divide = function(a,b){
+function divide(a,b){
     return a/b;
 }
+
+function operate(a, b, callback){
+    return callback(a,b)
+}
+
+buttons = document.querySelector("#buttons");
+// for (i=0;i<10;i++){
+//     button = document.createElement("button");
+//     button.textContent = i;
+//     button.id = i;
+//     buttons.appendChild(button);
+// }
